e3dae923a3b5cb967752ccc222aa057a20dec830
Maxime Leriche <maxleriche.60@gmail.com>
2022-10-05 13:34:35 +0200

    Finalise Channel model

--- server/model/channel.go
+++ server/model/channel.go
@@ -14,11 +14,33 @@ type Channel struct {
 	Messages    []Message
 }
 
-type ChannelUser struct {
-	gorm.Model
-	ChannelID uint
-	UserID    uint
-	CanSend   bool
-	CanRead   bool
-	CanMod    bool
+// Generate a new passkey for a channel
+func (c *Channel) GeneratePassKey() {
+	c.PassKey = RandomString(5)
+}
+
+// Get One Channel
+func (c *Channel) GetChannel(db *gorm.DB, id uint) error {
+	err := db.Model(&Channel{}).Preload("Users").Preload("Messages").First(c, id).Error
+	return err
+}
+
+// Get All Channels
+func (c *Channel) GetChannels(db *gorm.DB) (*[]Channel, error) {
+	var channels []Channel
+	err := db.Model(&Channel{}).Preload("Users").Preload("Messages").Find(&channels).Error
+	return &channels, err
+}
+
+// Delete one Channel
+func (c *Channel) DeleteChannel(db *gorm.DB, id uint) error {
+	err := db.Delete(c, id).Error
+	return err
+}
+
+// Create one Channel
+func (c *Channel) CreateChannel(db *gorm.DB) error {
+	c.GeneratePassKey()
+	err := db.Create(c).Error
+	return err
 }
