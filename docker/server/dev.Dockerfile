912a3f431a730083f49d65a2726dc18c35c68436
Maxime Leriche <maxleriche.60@gmail.com>
2022-10-06 02:55:32 +0200

    Setup swagger

--- docker/server/dev.Dockerfile
+++ docker/server/dev.Dockerfile
@@ -2,6 +2,7 @@ FROM golang:1.19-alpine
 WORKDIR /app
 
 RUN go install github.com/cosmtrek/air@latest
+RUN go install github.com/swaggo/swag/cmd/swag@latest
 
 ENV PORT=3001
 EXPOSE ${PORT}
--- server/main.go
+++ server/main.go
@@ -1,17 +1,33 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 
+	_ "batleforc/bipper/docs"
+
 	"github.com/joho/godotenv"
+	"github.com/labstack/echo/v4"
+	"github.com/swaggo/echo-swagger"
 )
 
+// @title Bipper Api
+// @version 1.0
+// @description Bipper api
+
+// @contact.name Batleforc
+// @contact.url https://weebo.fr
+// @contact.email maxleriche.60@gmail.com
+
+// @BasePath /api
 func main() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
+	e := echo.New()
+	e.GET("/swagger/*", echoSwagger.WrapHandler)
 
-	log.Println(os.Getenv("PORT"))
+	e.Logger.Fatal(e.Start(fmt.Sprintf(":%s", os.Getenv("PORT"))))
 }
